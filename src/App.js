e7f78e0e8773260ff4afe1f642616029621eb8c4
MadNake <72208527+MadNake@users.noreply.github.com>
2023-06-07 19:04:31 +0200

    added withRouter to profile pages

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 			<Navbar />
 			<div className='app-wrapper__content'>
 				<Routes>
-					<Route path='/profile/*' element={<ProfileContainer />} />
+					<Route path='/profile/:profileId?' element={<ProfileContainer />} />
 					<Route path='/dialogs/*' element={<DialogsContainer />} />
 					<Route path='/users' element={<UsersContainer />} />
 				</Routes>
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,16 @@ import axios from "axios";
 import Profile from "./Profile";
 import { connect } from "react-redux";
 import { setUserProfile } from "../../redux/profile-reducer";
+import { useLocation,	useNavigate, useParams} from "react-router-dom";
 
 class ProfileContainer extends React.Component {
 
 	componentDidMount() {
-		axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+		let profileId = this.props.router.params.profileId;
+		if (!profileId) {
+			profileId = 2;
+		};
+		axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${profileId}`)
 		.then(response => {
 			this.props.setUserProfile(response.data)
 		});
@@ -22,6 +27,22 @@ class ProfileContainer extends React.Component {
 
 let mapStateToProps = (state) => ({
  profile: state.profilePage.profile
-})
+});
 
-export default connect(mapStateToProps, {setUserProfile}) (ProfileContainer)
+function withRouter(Component) {
+	function ComponentWithRouterProp(props) {
+			let location = useLocation();
+			let navigate = useNavigate();
+			let params = useParams();
+			return (
+					<Component
+							{...props}
+							router={{ location, navigate, params }}
+					/>
+			);
+	}
+
+	return ComponentWithRouterProp;
+}
+
+export default connect(mapStateToProps, {setUserProfile}) (withRouter(ProfileContainer))
