5c5437db53e31c4c289d15cd2b57900357167323
MadNake <72208527+MadNake@users.noreply.github.com>
2023-06-13 20:52:29 +0200

    changed statusbar

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,19 +1,21 @@
 import Preloader from "../../common/Preloader/Preloader"
 import s from "./ProfileInfo.module.css"
 import userPhoto from "../../../assets/images/user.png"
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
 
 	if (!props.profile) {
 		return <Preloader/>
 	}
-
+debugger
 	return (
 		<div className={s.profileInfo__wrapper}>
 			<img className={s.profile__image} src={props.profile.photos.large ? props.profile.photos.large : userPhoto} alt='main-img'></img>
 			<h1 className={s.profile__name}>{props.profile.fullName}</h1>
 			<img className={s.profileAva__image} src={props.profile.photos.small ? props.profile.photos.small : userPhoto} alt='main-img'></img>
-			<p className={s.profile__description}>{props.profile.aboutMe}</p>
+			<ProfileStatus status={props.profile.aboutMe}/>
+			{/* <p className={s.profile__description}>{props.profile.aboutMe}</p> */}
 		</div>
 	)
 }
